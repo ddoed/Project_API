da2b93b762b49f6ef253e3d843b054bc6e7261eb
Mireutale <162964181+Mireutale@users.noreply.github.com>
2025-02-11 10:29:22 +0900

    class product add cascade = "all, delete"

--- app/models/models.py
+++ app/models/models.py
@@ -47,15 +47,15 @@ class Product(SQLModel, table=True):
     category_id: int = Field(foreign_key="category.id")
     soldout: bool = False
 
-    purchases: List["Purchase"] = Relationship(back_populates="product") #임시 추가
     user: User = Relationship(back_populates="products")
     category: Category = Relationship(back_populates="products")
-    images: List["ProductImage"] = Relationship(back_populates="product")
-    likes: List["Likes"] = Relationship(back_populates="product")
-    comments: List["Comment"] = Relationship(back_populates="product")
+    images: List["ProductImage"] = Relationship(back_populates="product", sa_relationship_kwargs={"cascade": "all, delete"})
+    likes: List["Likes"] = Relationship(back_populates="product", sa_relationship_kwargs={"cascade": "all, delete"})
+    comments: List["Comment"] = Relationship(back_populates="product", sa_relationship_kwargs={"cascade": "all, delete"})
     # ! Product 모델에 purchases라는 관계가 없는데, Purchase 모델에서 Product와의 관계를 설정하려고 했기 때문에 에러 발생
     # // Product 모델에 purchases Relationship을 추가해서 에러 수정
-    purchases: List["Purchase"] = Relationship(back_populates="product")
+    purchases: List["Purchase"] = Relationship(back_populates="product", sa_relationship_kwargs={"cascade": "all, delete"})
+
 
 class ProductImage(SQLModel, table=True):
     # * 하나의 게시물에 여러개의 사진이 연결될 수 있도록 id를 설정
