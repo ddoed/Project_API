c80ef60fba3d858c1249c729b3a946194ae31852
yimnabin <yimnabin20051022@gmail.com>
2025-02-11 15:56:51 +0900

    refactor: remove email and name requirement
    - AuthLoginReq에서 로그인할때 email과 name을 요구하지 않도록 변경

--- app/models/auth_models.py
+++ app/models/auth_models.py
@@ -11,15 +11,13 @@ class user_LikeRequest(BaseModel):
 class AuthLoginReq(BaseModel):
     login_id:str
     pwd:str
-    name:str
-    email:EmailStr
 
 class AuthSignupReq(BaseModel):
     login_id:str
     pwd:str
     name:str
     email:EmailStr
-    
+
 class ProfileUpdateRequest(BaseModel):
     username: str | None = Field(None, min_length=2, max_length=50)
     email: EmailStr | None = None
